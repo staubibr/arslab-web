75fdb8cde87b493608bb41c32f292731acac9ccf
staubibr <bruno.st-aubin2@canada.ca>
2020-04-25 07:41:41 -0400

    in progress

--- application.js
+++ application.js
@@ -16,10 +16,12 @@ import Settings from '../web-devs-tools/widgets/settings.js';
 
 export default class Main extends Templated { 
 
+	get Settings() { return this.Elem("settings").Settings; }
+
 	constructor(node) {		
 		super(node);
 		
-		this.popup = new Popup();
+		this.popup = new Popup(this.Elem('main'));
 		
 		this.widgets = {
 			converter : new Converter(),
@@ -27,14 +29,21 @@ export default class Main extends Templated {
 			settings : new Settings()
 		}
 		
-		this.Node('upload').Icon = 'fa-file-upload';
-		this.Node('upload').Label = Core.Nls('Dropzone_Upload_Label');
-		this.Node('upload').Label = Core.Nls('Dropzone_Upload_Label');
+		this.Elem('upload').Icon = 'fa-file-upload';
+		this.Elem('upload').Label = Core.Nls('Dropzone_Upload_Label');
+		this.Elem('upload').Label = Core.Nls('Dropzone_Upload_Label');
 		
-		this.Node('convert').addEventListener('click', this.OnButtonConvert_Click.bind(this));
-		this.Node('rise').addEventListener('click', this.OnButtonRise_Click.bind(this));
-		this.Node('settings').addEventListener('click', this.OnButtonSettings_Click.bind(this));
+		this.Elem('convert').addEventListener('click', this.OnButtonConvert_Click.bind(this));
+		this.Elem('rise').addEventListener('click', this.OnButtonRise_Click.bind(this));
+		this.Elem('settings').addEventListener('click', this.OnButtonSettings_Click.bind(this));
+		
+		this.widgets.converter.On("converted", this.OnConverter_Converted.bind(this));
+		this.widgets.converter.On("error", this.OnWidget_Error.bind(this));
+		
+		this.widgets.rise.On("FilesReady", this.OnRise_Loaded.bind(this));
+		this.widgets.rise.On("error", this.OnWidget_Error.bind(this));
 	}
+	// this.Elem("playback").Initialize(this.simulation, this.Settings);
 	
 	OnButtonRise_Click(ev) {
 		this.ShowPopup(Core.Nls("Popup_Rise_Title"), this.widgets.rise, 'rise-loader');
@@ -48,17 +57,35 @@ export default class Main extends Templated {
 		this.ShowPopup(Core.Nls("Popup_Settings_Title"), this.widgets.settings, 'settings');
 	}
 	
+	OnConverter_Converted(ev) {
+		this.HidePopup();
+	}
+	
+	OnRise_Loaded(ev) {
+		this.HidePopup();
+		
+		this.Elem("upload").Update(ev.files);
+	}
+	
+	OnWidget_Error(ev) {
+		alert (ev.error);
+	}
+	
 	ShowPopup(title, widget, css) {
 		this.popup.Widget = widget;
 		this.popup.Title = title;
 		
-		this.popup.SetCss(`popup ${css}`);
+		this.popup.SetCss(`popup popup-${css}`);
 		
 		this.popup.Show();
 	}
 	
+	HidePopup() {
+		this.popup.Hide();
+	}
+	
 	Template() {
-		return	"<main>" +
+		return	"<main handle='main'>" +
 					"<div handle='header' widget='Widget.Header'></div>" +
 					
 					"<div class='dropzone-container'>" + 
@@ -67,7 +94,7 @@ export default class Main extends Templated {
 					   "<div class='dropzone-column'>" + 
 						  "<button handle='convert' title='nls(Dropzone_Convert)' class='fas fa-exchange-alt'></button>" +
 						  "<button handle='rise' title='nls(Dropzone_Rise)' class='fas fa-cloud-download-alt'></button>" +
-						  "<button handle='settings' title='nls(Playback_Settings)' class='fas fa-tools' disabled></button>" +
+						  "<button handle='settings' title='nls(Playback_Settings)' class='fas fa-tools'></button>" +
 					   "</div>" +
 					"</div>" +
 					
