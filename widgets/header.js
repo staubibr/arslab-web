d04adbd2e7e00073a8362c3316d6c1a0e4ec5fdf
staubibr <bruno.st-aubin2@canada.ca>
2020-05-04 10:02:34 -0400

    initial

---
+++ widgets/header.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import Core from '../../basic-tools/tools/core.js';
+import Templated from '../../basic-tools/components/templated.js';
+
+export default Core.Templatable("Widget.Header", class Header extends Templated { 
+
+	constructor(container) {
+		super(container);		
+	}
+	
+	Template() {
+		return "<h1 class='first-row'><a href='http://vs3.sce.carleton.ca/wordpress/' target='_blank'>nls(Header_Lab)</a></h1>" +
+			   "<div class='second-row'>" +
+				  "<h2><a href='http://cell-devs.sce.carleton.ca/intranet/webviewer/' target='_blank'>nls(Header_App)</a></h2>" +
+				  "<div class='links-container'>" +
+					"<h2>nls(App_Title)</h2>" +
+				  "</div>" +
+			   "</div>";
+	}
+});
